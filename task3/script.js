33c1e65e1ac283d326c64ebcebe522c7d03b7f5a
FolayemiASC21 <fkehinde@brooklynfriends.org>
2021-08-12 18:14:15 -0400

    first commit

--- task3/script.js
+++ task3/script.js
@@ -1,1 +1,15 @@
 // Your code goes here:
+
+let button = document.querySelector("#race")
+let pikachu = document.querySelector("#pikachu")
+let sonic = document.querySelector("#sonic")
+
+button.onclick = function(event) {
+    event.preventDefault()
+
+    let pRandNum = Math.floor(Math.random() * (5 - 1+1)+1)
+    let sRandNum = Math.floor(Math.random() * (5 - 1+1)+1)
+
+    pikachu.style.animation = "racerSpeed " + pRandNum.toString() + "s linear"
+    sonic.style.animation = "racerSpeed " + sRandNum.toString() + "s linear"
+}
